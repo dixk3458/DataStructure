9f9679456be51156f0144aecbf04d5796296c25b
Jaewoong <dixk3458@naver.com>
2023-10-14 23:44:13 +0900

    Stack(스택)

---
+++ Stack/array_stack.js
@@ -0,0 +1,13 @@
+const stack = [];
+
+// Push
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+// Pop
+stack.pop(); //3
+console.log(stack); //[1,2]
+
+// Get top
+console.log(stack[stack.length - 1]); // 2
---
+++ Stack/linked_list_stack.js
@@ -0,0 +1,38 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    newNode.next = this.top;
+    this.top = newNode;
+    this.size += 1;
+  }
+
+  pop() {
+    const value = this.top.value;
+    this.top = this.top.next;
+    this.size -= 1;
+    return value;
+  }
+
+  size() {
+    return this.size;
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack.pop());
+stack.push(4);
