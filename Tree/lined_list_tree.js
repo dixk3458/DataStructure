bbaf2002268d1968640e6ecc01987905dac4a013
Jaewoong <dixk3458@naver.com>
2023-10-16 20:29:40 +0900

    Tree(트리)

---
+++ Tree/lined_list_tree.js
@@ -0,0 +1,19 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(node) {
+    this.root = node;
+  }
+}
+
+const ree = new Tree(new Node(9));
+tree.root.left = new Node(3);
+tree.root.right = new Node(8);
+tree.root.left.left = new Node(2);
+tree.root.left.right = new Node(5);
